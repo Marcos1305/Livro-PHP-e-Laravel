d29f553c7db0475897c3ab59600bf07f412e2fc7
marcos1305 <marcos1305pt@gmail.com>
2018-06-11 17:03:48 -0300

    Capitulo 4

--- livro_laravel/database/seeds/DatabaseSeeder.php
+++ livro_laravel/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+        $this->call('ProdutoTableSeeder');
+    }
+
+}
+class ProdutoTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::insert('INSERT INTO produtos(nome, quantidade, valor, descricao) VALUES (?,?,?,?)', ['Geladeira', 2, 5.900, 'Side by Side com gelo na porta']);
+        DB::insert('INSERT INTO produtos(nome, quantidade, valor, descricao) VALUES (?,?,?,?)', ['Fogão', 5, 950.00, 'Painel automático e forno elétrico']);
+        DB::insert('INSERT INTO produtos(nome, quantidade, valor, descricao) VALUES (?,?,?,?)', ['Microondas', 1, 1520.00, 'Manda SMS quando termina de esquentar']);
+
     }
 }
---
+++ livro_laravel/database/seeds/ProdutoTableSeeder.php
@@ -0,0 +1,15 @@
+<?php 
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class ProdutoTableSeeder extends Seeder
+{
+    public function run()
+    {
+        Model::unguard();
+    }
+
+  
+}
+
+?>
